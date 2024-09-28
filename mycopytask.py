28cd379e9b6643b15ea615ec2fd5fe45eb11e74f
RolanRolanE <rolanjayisabida750@gmail.com>
2024-09-28 10:32:10 +0800

    yes mana ko salamat pasar nako

---
+++ mycopytask.py
@@ -0,0 +1,7 @@
+#print negative, print positive
+age = [-12,13,-14,15,-16]
+mana ko copy
+
+#if james exist in the array print james cute or else james none
+username = ["juan", "pedro", "james", "sagaad", "fuego"]
+mana ko copy
